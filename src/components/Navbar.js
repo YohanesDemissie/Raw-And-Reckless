ca5b0fc039c2c22c582e0f07b7e95f7993deeb23
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-11-02 04:33:35 -0700

    added basic styling and li tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,19 @@ class Navbar extends Component {
   render() {
     return(
       <div className="navbar">
-        <h1>
+        <h1 className="title">
           Raw and Reckless
         </h1>
+        <hr />
+
+        <ul className="products">
+          <li>Bath</li>
+            <li>Shower</li>
+              <li>Hair</li>
+                <li>Body</li>
+                  <li>Face</li>
+                    <li>Fragrance</li>
+        </ul>
       </div>
     )
   }
