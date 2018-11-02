3d308fc0cd574b5a43117cc549bf5f137fc4f9b3
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-11-02 04:45:17 -0700

    added image place holder

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import '../styles/Home.css'
 import Navbar from './Navbar'
+import placeHolder from '../images/placeholder.jpg'
 
 class Home extends Component{
   render() {
     return(
       <div>
         <Navbar />
+        <img className="homeImage" src={placeHolder} alt="black beauty" />
       </div>
     )
   }
